6fbb87de862f525ca61fdd8ef4d3b61d52756032
Ruslan Uteshev <r.s.uteshev@gmail.com>
2023-04-29 19:59:03 +0400

    feat: add Keyboard component and emulate native behaviour for keyboard events

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import Key from "./Key.js"
+
+const arrowKeys = ['ArrowRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown']
+
 const specialChars  = {
   'Tab': '\t',
   'Enter': '\n'
@@ -20,30 +24,206 @@ function createTextareaEl() {
 }
 
 function listenInput() {
+  document.querySelector('#textarea').focus()
   document.addEventListener('keydown', (e) => {
-    console.log("Keydown event", e)
-    if(!['Shift', 'Meta', 'Control', 'Alt', 'Tab', 'Enter', 'Backspace'].includes(e.key)) {
-      printSymbol(e.key)
-    } else {
+
+    const textareaEl = document.querySelector('#textarea')
+
+    const curPos = textareaEl.selectionStart
+    const selectionEnd = textareaEl.selectionEnd
+    console.log(selectionEnd, '|||', selectionEnd === curPos, curPos)
+    if (!(['Shift', 'Meta', 'Control', 'Alt', 'CapsLock'].includes(e.key) ||  e.metaKey ||  arrowKeys.includes(e.key))) {
+      console.log(e)
+      console.log(e.metaKey)
       e.preventDefault()
+      printSymbol(e.key)
     }
     
+    if(['Shift', 'Meta', 'Control', 'Alt', 'Tab', 'Enter', 'Backspace', 'CapsLock'].includes(e.key)) {
+      e.preventDefault()
+    }
+    document.querySelector('#textarea').focus()
   })
+  
 }
 
 function printSymbol(keyChar) {
+  /* console.log(keyChar) */
+  const textareaEl = document.querySelector('#textarea')
+  // get currentPosition
+  const curPos = textareaEl.selectionStart
+  const selectionEnd = textareaEl.selectionEnd
+  console.log(selectionEnd, '|||', selectionEnd === curPos, curPos)
+  const beforeCursorInput = textareaEl.value.slice(0, curPos) || ''
+  const afterCursorInput = textareaEl.value.slice(selectionEnd) || ''
+  console.log(curPos)
   let char = ''
-  if(['Tab', 'Enter', 'Backspace'].includes(keyChar)) {
+  if(['Shift', 'Meta', 'Control', 'Alt', 'Tab', 'Enter', 'Backspace', 'CapsLock'].includes(keyChar)) {
     if (keyChar === 'Backspace') {
-      textarea.value = textarea.value.slice(0, -1)
+      /* console.log(keyChar) */
+      
+      console.log(selectionEnd)
+     if (curPos !== selectionEnd) {
+      let selectedStart, selectedEnd
+      if (curPos > selectionEnd) {
+        selectedStart = selectionEnd
+        selectedEnd = curPos
+      } else {
+        selectedStart = curPos
+        selectedEnd = selectionEnd
+      }
+        const afterSelection = textareaEl.value.slice(selectedEnd) || ''
+        const beforeSelection = textareaEl.value.slice(0, selectedStart) || ''
+        textareaEl.value = `${beforeSelection}${afterSelection}`
+        textareaEl.setSelectionRange(selectedStart, selectedStart)
+        return
+      } else {
+        
+      const newBeforeCursor = beforeCursorInput.slice(0, curPos - 1) || ''
+      textareaEl.value = `${newBeforeCursor}${afterCursorInput}`
+      textareaEl.setSelectionRange(curPos - 1, curPos - 1)
+       }
       return
-    } else {
+    } else if (['Shift', 'Meta', 'Control', 'Alt'].includes(keyChar)) { 
+      return
+    }else {
       char = specialChars[keyChar]
+      
     }
   } else {
     char = keyChar
   }
+ 
+  
+  // inssert into Current Position
+  textareaEl.value = `${beforeCursorInput}${char}${afterCursorInput}`
+  textareaEl.setSelectionRange(curPos + 1, curPos + 1)
 
-  const textarea = document.getElementById('textarea')
-  textarea.value += char
 }
+
+const row = [
+  {
+    code: 1,
+    base: 'Tab',
+    alter: 'Tab'
+  },
+  {
+    code: 2,
+    base: 'q',
+    alter: 'Q'
+  },
+  {
+    code: 3,
+    base: 'w',
+    alter: 'W'
+  },
+  {
+    code: 4,
+    base: 'e',
+    alter: 'E'
+  },
+  {
+    code: 5,
+    base: 'r',
+    alter: 'R'
+  },
+  {
+    code: 6,
+    base: 't',
+    alter: 'T'
+  },
+  {
+    code: 7,
+    base: 'y',
+    alter: 'Y'
+  },
+  {
+    code: 8,
+    base: 'u',
+    alter: 'U'
+  },
+  {
+    code: 9,
+    base: 'i',
+    alter: 'I'
+  },
+  {
+    code: 10,
+    base: 'o',
+    alter: 'O'
+  },
+  {
+    code: 11,
+    base: 'p',
+    alter: 'P'
+  },
+  {
+    code: 12,
+    base: '[',
+    alter: '{'
+  },
+  {
+    code: 13,
+    base: ']',
+    alter: '}'
+  },
+  {
+    code: 14,
+    base: '\\',
+    alter: '|'
+  }
+]
+
+
+class Keyboard {
+  constructor(lang) {
+    this.lang = lang
+    this.isCapsed = false
+    this.layout = 'Tab q w e r t y u i o p [ ] \\'
+  }
+
+  render() {
+    let rowHtml = row.map((obj) => {
+      const keyObj = new Key(obj)
+      return keyObj.render()
+    })
+
+    const keyboardHtml = `<div id="keyboard">${rowHtml.join('')}</div>`
+    document.body.insertAdjacentHTML('beforeend', keyboardHtml)
+  }
+
+  switchOnCaps(capsState) {
+    const keys = document.getElementById('keyboard').querySelectorAll('.key')
+        this.isCapsed = capsState
+        keys.forEach((key) => {
+          if (this.isCapsed === true) {
+            key.textContent = key.dataset.alter
+          } else {
+            key.textContent = key.dataset.base
+          }
+        })
+  }
+
+  listenCaps() {
+    console.log('listenng')
+    window.addEventListener("keydown", (e) => {
+      if (e.key === 'CapsLock') {
+        const capsState = e.getModifierState('CapsLock')
+        this.switchOnCaps(capsState) 
+      }
+    });
+    window.addEventListener("keyup", (e) => {
+      if (e.key === 'CapsLock') {
+        const capsState = e.getModifierState('CapsLock')
+        this.switchOnCaps(capsState) 
+      }
+    });
+    
+  }
+}
+
+const keyboard = new Keyboard()
+
+keyboard.render()
+keyboard.listenCaps() 
+
